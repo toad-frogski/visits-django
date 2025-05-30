05cf530c47950d2af8c2b8db5e62100636e0a110
Nichita Muntean <nichita.muntean@deeplace.md>
2025-05-30 17:07:58 +0300

    enchance

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,20 +1,47 @@
+import hashlib
 from rest_framework import serializers
+from rest_framework.request import Request
 from django.contrib.auth.models import User
 from visits.serializers import SessionModelSerializer
 from .models import UserProfile
 
 
 class UserProfileModelSerializer(serializers.ModelSerializer):
+    avatar = serializers.ImageField(write_only=True, required=False)
+    avatar_url = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = UserProfile
-        fields = ["avatar"]
+        fields = ["avatar_url", "avatar"]
+
+    def get_avatar_url(self, obj: UserProfile):
+        request: Request = self.context.get("request")
+        if obj.avatar:
+            return request.build_absolute_uri(obj.avatar.url)
+
+        email = (obj.user.email or "").strip().lower()
+        if not email:
+            return None
+
+        email_hash = hashlib.md5(email.encode("utf-8")).hexdigest()
+
+        return f"https://www.gravatar.com/avatar/{email_hash}"
 
 
 class UserModelSerializer(serializers.ModelSerializer):
     profile = UserProfileModelSerializer(allow_null=True)
+    full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ["id", "first_name", "last_name", "profile"]
+        fields = ["id", "full_name", "profile"]
+
+    def get_full_name(self, obj: User):
+        return (
+            f"{(obj.first_name or "").capitalize()} {(obj.last_name or "").capitalize()}"
+            if obj.first_name and obj.last_name
+            else f"{obj.username.capitalize()}"
+        )
 
 
 class UserSessionSerializer(serializers.Serializer):
