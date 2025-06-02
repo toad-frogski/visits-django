9bd3ff9f3715ea3b8cb41923537ad0da0d5d3a88
Nichita Muntean <nichita.muntean@deeplace.md>
2025-06-02 17:09:26 +0300

    verify token

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from visits import urls as visits_urls
@@ -16,6 +16,7 @@ urlpatterns = [
     # jwt token
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     # app routes
     path("api/v1/visits/", include(visits_urls.urlpatterns)),
     path("api/v1/rfid/", include(rfid_urls.urlpatterns)),
