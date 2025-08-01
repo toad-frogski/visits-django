422eecf8afa32d342cccbed01b4e7916765de8f8
Nichita Muntean <nichita.muntean@deeplace.md>
2025-08-01 14:30:34 +0300

    prepare for deploy

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,20 +1,17 @@
+import AppSidebar from "@/app/components/app-sidebar";
+import MobileNav from "@/app/components/mobile-nav";
 import { SidebarProvider } from "@/shared/components/ui/sidebar";
 import { useIsMobile } from "@/shared/hooks/use-mobile";
-import { Suspense, lazy, type FC } from "react";
+import type { FC } from "react";
 import { Outlet } from "react-router-dom";
-
-const AppSidebar = lazy(() => import("@/app/components/app-sidebar"));
-const MobileNav = lazy(() => import("@/app/components/mobile-nav"));
 
 const App: FC = () => {
   const isMobile = useIsMobile();
 
   return (
     <SidebarProvider defaultOpen={false}>
-      <Suspense fallback={null}>
-        {!isMobile && <AppSidebar />}
-        {isMobile && <MobileNav />}
-      </Suspense>
+      {!isMobile && <AppSidebar />}
+      {isMobile && <MobileNav />}
       <main className="w-full p-3 md:p-6 pb-24 md:pb-6">
         <Outlet />
       </main>
